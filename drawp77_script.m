dc932c4414cb7c1f0a6d9308870086c7fde0923b
moueza <>
2023-02-15 07:32:25 +0100

    feat(mesh): sov

--- drawp77_script.m
+++ drawp77_script.m
@@ -1,5 +1,9 @@
-%p32
-a=1
-  b=2
-  c=a+b
-  c
+%z=linspace(0,1000,100);
+
+%c_roots=[0 1]
+ % p=polyval(c_roots,z)
+
+  % X=linspace(0,1000,100);
+X=[1 2 3]
+Y=[1 4 9]
+  plot(X,Y)
---
+++ plot3DmeshWave_script.m
@@ -0,0 +1,15 @@
+%p5-52 of Matlab manual
+
+%X=linspace(-1,1,10);
+%Y=linspace(-1,1,10);
+
+%X=[1 2 3 4]
+%Y=[1 4 9 16] 
+
+  [X,Y]= meshgrid(-10:1:10);
+
+R=sqrt(X.^2 + Y.^2);
+Z = sin(R)./R;
+
+ % plot3(X,Y,Z)
+   mesh(X,Y,Z,'EdgeColor','red')
